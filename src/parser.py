0165ef5d7609133d3ca462bfddfc6ce286b45495
Federico <belerico@pop-os.localdomain>
2019-09-12 20:51:08 +0200

    Update hmm

--- src/parser.py
+++ src/parser.py
@@ -74,13 +74,24 @@ def save_pickle(obj, path):
 # save_pickle(parsed_dataset, '../dataset/parsed_dataset.pkl')
 vocabs = load_pickle(os.path.join(DATA_DIR, 'vocab.pkl'))
 parsed_dataset = load_pickle(os.path.join(DATA_DIR, 'parsed_dataset.pkl'))
-obs_states = [numpy.array([state for state in chorale]) for chorale in parsed_dataset]
-obs_states = [state.reshape(-1, 1) for state in obs_states]
-lengths = [len(seq) for seq in obs_states]
+
+training_set = parsed_dataset[:30]
+test_set = parsed_dataset[30:66]
+
+obs_train = [numpy.array([state for state in chorale]) for chorale in training_set]
+obs_train = [state.reshape(-1, 1) for state in obs_train]
+train_lengths = [len(seq) for seq in obs_train]
+
+obs_test = [numpy.array([state for state in chorale]) for chorale in test_set]
+obs_test = [state.reshape(-1, 1) for state in obs_test]
+test_lengths = [len(seq) for seq in obs_test]
+
 # Train the model.
-model = hmm.MultinomialHMM(n_components=3, n_iter=100)
+hmm.MultinomialHMM._check_input_symbols = lambda *_: True
+model = hmm.MultinomialHMM(n_components=25, n_iter=100)
 model.monitor_.verbose = True
-# model.n_features = len(vocabs)
-model.fit(numpy.concatenate(obs_states), lengths)
+model.n_features = len(vocabs)
+model.fit(numpy.concatenate(obs_train), train_lengths)
+print(model.score(numpy.concatenate(obs_test), test_lengths))
 # Z2 = model.predict(obs_states)
 # print(Z2)
