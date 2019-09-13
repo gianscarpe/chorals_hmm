d45201c817deefd1b1a0c7a870995fd8c1a13bce
belerico <belo.fede@outlook.com>
2019-09-13 11:25:30 +0200

    Fix generation of parsed dataset considering only pitch and dur

--- main_hmm.py
+++ main_hmm.py
@@ -1,4 +1,5 @@
 import os
+import math
 import pickle
 import numpy
 import itertools
@@ -10,7 +11,7 @@ from functools import reduce
 vocabs = load_pickle(os.path.join(DATA_DIR, 'vocab.pkl'))
 parsed_dataset = load_pickle(os.path.join(DATA_DIR, 'parsed_dataset.pkl'))
 
-training_set = parsed_dataset
+training_set = parsed_dataset[:60]
 test_set = parsed_dataset[60:]
 
 obs_train = [numpy.array([state for state in chorale]) for chorale in training_set]
@@ -22,12 +23,13 @@ obs_test = [state.reshape(-1, 1) for state in obs_test]
 test_lengths = [len(seq) for seq in obs_test]
 
 # Train the model.
-# hmm.MultinomialHMM._check_input_symbols = lambda *_: True
-model = hmm.MultinomialHMM(n_components=25, n_iter=200)
+hmm.MultinomialHMM._check_input_symbols = lambda *_: True
+model = hmm.MultinomialHMM(n_components=10, n_iter=100)
 model.monitor_.verbose = True
 model.n_features = len(vocabs)
 model.fit(numpy.concatenate(obs_train), train_lengths)
-likelihoods = [model.score(sequence) for sequence in obs_test]
-print(model.score(numpy.concatenate(obs_test), test_lengths))
+likelihoods = [model.score(sequence[:10]) for i, sequence in enumerate(obs_test)]
+print(likelihoods)
+# print(model.score(numpy.concatenate(obs_test), test_lengths))
 # Z2 = model.predict(obs_states)
 # print(Z2)
