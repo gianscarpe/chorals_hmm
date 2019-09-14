80fbbcbe6df6f093fe2269d770ec493b6ae06845
Federico <belerico@pop-os.localdomain>
2019-09-14 20:54:20 +0200

    Compute state space

--- main_hmm.py
+++ main_hmm.py
@@ -4,15 +4,15 @@ import pickle
 import numpy
 import itertools
 from src import BASE_DIR, DATA_DIR
-from src.helpers import *
+from src.helpers import load_pickle
 from hmmlearn import hmm
 from functools import reduce
 
 vocabs = load_pickle(os.path.join(DATA_DIR, 'bach_chorales', 'vocab.pkl'))
 parsed_dataset = load_pickle(os.path.join(DATA_DIR, 'bach_chorales', 'parsed_dataset.pkl'))
 
-training_set = parsed_dataset[:60]
-test_set = parsed_dataset[60:]
+training_set = parsed_dataset[:90]
+test_set = parsed_dataset[90:]
 
 obs_train = [numpy.array([state for state in chorale]) for chorale in training_set]
 obs_train = [state.reshape(-1, 1) for state in obs_train]
@@ -28,7 +28,7 @@ model = hmm.MultinomialHMM(n_components=10, n_iter=100)
 model.monitor_.verbose = True
 model.n_features = len(vocabs)
 model.fit(numpy.concatenate(obs_train), train_lengths)
-likelihoods = [model.score(sequence[:10]) for i, sequence in enumerate(obs_test)]
+likelihoods = [model.score(sequence) for i, sequence in enumerate(obs_test)]
 print(likelihoods)
 # print(model.score(numpy.concatenate(obs_test), test_lengths))
 # Z2 = model.predict(obs_states)
