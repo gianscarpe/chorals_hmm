f532abd8f8cd9da4f13e0879264c2312012bcf23
Federico <belerico@pop-os.localdomain>
2019-09-14 23:29:14 +0200

    Generate midi song and play it

--- main_hmm.py
+++ main_hmm.py
@@ -3,8 +3,9 @@ import math
 import pickle
 import numpy
 import itertools
-from src import BASE_DIR, DATA_DIR
-from src.helpers import load_pickle
+from src import BASE_DIR, DATA_DIR, MODELS_DIR
+from src.parser.parser import states2notes
+from src.helpers import load_pickle, save_pickle, notes2midi
 from hmmlearn import hmm
 from functools import reduce
 
@@ -24,12 +25,26 @@ test_lengths = [len(seq) for seq in obs_test]
 
 # Train the model.
 hmm.MultinomialHMM._check_input_symbols = lambda *_: True
-model = hmm.MultinomialHMM(n_components=10, n_iter=100)
-model.monitor_.verbose = True
-model.n_features = len(vocabs)
-model.fit(numpy.concatenate(obs_train), train_lengths)
+n_components = 20
+n_iter = 100
+train = True
+model_name = 'hmm_' + str(n_components) + '_' + str(n_iter) + '.pkl'
+
+if train:
+    model = hmm.MultinomialHMM(n_components=n_components, n_iter=n_iter)
+    model.monitor_.verbose = True
+    model.n_features = len(vocabs)
+    model.fit(numpy.concatenate(obs_train), train_lengths)
+    save_pickle(model, os.path.join(MODELS_DIR, 'hmm', model_name))
+else:
+    model = load_pickle(os.path.join(MODELS_DIR, 'hmm', model_name))
+
+# Print likelihoods
 likelihoods = [model.score(sequence) for i, sequence in enumerate(obs_test)]
 print(likelihoods)
-# print(model.score(numpy.concatenate(obs_test), test_lengths))
-# Z2 = model.predict(obs_states)
-# print(Z2)
+
+# Generate song
+sample, _ = model.sample(50, 42)
+sample = list(itertools.chain(*sample))
+notes = states2notes(sample, vocabs)
+notes2midi(notes, 'generated.mid')
--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ import itertools
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', 'dataset'))
+MODELS_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', 'models'))
 
 # Bach chorales state space: pitch x duration
 MIN_PITCH = 60
