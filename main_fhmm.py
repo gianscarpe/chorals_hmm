2cb12534e702fae528faed9251cdc255192a04fa
belerico <belo.fede@outlook.com>
2019-09-18 18:18:04 +0200

    Add callable main

--- main_fhmm.py
+++ main_fhmm.py
@@ -17,7 +17,7 @@ M: from 2 to 9
 '''
 
 
-def train(params, M, K, n_iterations, model_name, verbose, random_seed=1):
+def train(params, M, K, n_iterations, model_name, verbose, random_seed=42):
     print(" -- Training FHMM --\n")
     random_state = np.random.RandomState(random_seed)
     for i in range(M):
@@ -35,18 +35,15 @@ def train(params, M, K, n_iterations, model_name, verbose, random_seed=1):
 
     hmm = FullDiscreteFactorialHMM(params=params, n_steps=1000,
                                    calculate_on_init=True)
+    hmm = hmm.EM(trainset, n_iterations=n_iterations,
+                                    verbose=verbose)
+    return hmm
 
-    trained_hmm = hmm.EM(training_set, n_iterations=n_iterations,
-                         verbose=verbose)
-
-    return trained_hmm
-
-
-def test(model, test_set):
+def test(model, testset):
     print(" -- Testing FHMM --\n")
     log_likelihoods = np.array(
-        [model.Forward(np.array(test_set[i]))[2] for i, sequence in
-         enumerate(test_set)])
+        [model.Forward(np.array(testset[i]))[2] for i, sequence in
+         enumerate(testset)])
 
     print(f"Test likelihood {np.mean(log_likelihoods)}")
 
@@ -55,37 +52,59 @@ if __name__ == "__main__":
     print(" -- main.py FHMM --\n")
     parser = argparse.ArgumentParser(description='Train and test FHMM')
     parser.add_argument('-K', type=int, default=2, help='value K')
-    parser.add_argument('-M', type=int, default=2, help='value K')
+    parser.add_argument('-M', type=int, default=2, help='value M of states')
     parser.add_argument('-N', type=int, default=15, help='Number of iterations')
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="increase output verbosity")
-    parser.add_argument("-s", "--save_model", action="store_true",
+    parser.add_argument("-s", "--save-model", action="store_true",
                         help="Save the trained model")
+    parser.add_argument('--trainset-size', type=str, action='store',
+                        default='50',
+                        help='Training set size')
+    parser.add_argument('--skip-training', action='store_true')
+    parser.add_argument('--model-path', type=str, action='store',
+                        default=None,
+                        help='Load model instead of training a new one')
     parser.add_argument("--generate", action="store_true",
                         help="Generate a midi file")
-    parser.add_argument("--dataset", type=str,
-                        default=os.path.join(DATA_DIR, 'chorales', 'music21'),
+    parser.add_argument("--dataset-dir", type=str,
+                        default=os.path.join(DATA_DIR, 'music21'),
+                        help="Dataset base dir")
+    parser.add_argument("--trainset-name", type=str,
+                        default=os.path.join(DATA_DIR, 'music21', 'bach_states_dataset.pkl'),
+                        help="Dataset base dir")
+    parser.add_argument("--testset-name", type=str,
+                        default=None,
                         help="Dataset base dir")
 
     args = parser.parse_args()
 
-    DIR = args.dataset
-    vocabs = load_pickle(os.path.join(DIR, 'vocab.pkl'))
-    parsed_dataset = load_pickle(os.path.join(DIR,
-                                              'chorales_states_dataset.pkl'))
+    fullname = os.path.join(args.dataset_dir, args.trainset_name)
+    vocabs = load_pickle(os.path.join(args.dataset_dir, 'vocab.pkl'))
+    parsed_dataset = load_pickle(fullname)
 
     dataset = np.array(list(map(np.array, parsed_dataset)))
-    training_set = dataset[:60]  # parsed training set state
-    test_set = dataset[60:]
+    if args.trainset_size == 'all':
+        trainset_size = len(dataset)
+        trainset = dataset
+    else:
+        trainset_size = int(args.trainset_size)
+        trainset = dataset[:trainset_size]
+    if args.testset_name is not None:
+        testset = load_pickle(os.path.join(args.dataset_dir, args.testset_name))
+    else:
+        if trainset_size < len(dataset):
+            testset = dataset[:trainset_size]
+        else:
+            print('Not enough data for the test size')
+            exit(1)
 
-    n_steps = len(training_set)
-
+    n_steps = len(trainset)
     K = args.K
     M = args.M
     D = len(vocabs)  # what is it? maybe number of different observation
     n_iterations = args.N
     hmm_generate = args.generate
-
     save = args.save_model
     verbose = args.verbose
 
@@ -95,10 +114,16 @@ if __name__ == "__main__":
               'transition_matrices': np.zeros((M, K, K)),
               'obs_given_hidden': np.zeros([K] * M + [D])}
 
-    model_name = f"K-{K}-M-{M}-nit-{n_iterations}"
+    model_name = f"K-{K}-M-{M}-ts-{trainset_size}-nit-{n_iterations}"
 
-    trained_hmm = train(params, M, K, n_iterations, model_name, verbose)
-    test(trained_hmm, test_set)
+    if args.skip_training:
+        if args.model_path is None:
+            print('SPecify the model path running this command with --model-path PATH-TO-MODEL')
+            exit(1)
+        trained_hmm = load_pickle(os.path.abspath(args.model_path))
+    else:
+        trained_hmm = train(params, M, K, n_iterations, model_name, verbose)
+    test(trained_hmm, testset)
 
     if save:
         save_pickle(trained_hmm,
@@ -107,12 +132,15 @@ if __name__ == "__main__":
     if hmm_generate:
         print(" -- Generating MIDI --\n")
         _, sample = trained_hmm.Simulate()
-        sample = list(itertools.chain(*sample))
+        sample = list(itertools.chain(*sample))[:50]
         # notes = states2notes(sample, vocabs)
         # notes2midi(notes, os.path.join(MIDI_DIR, 'hmm', 'generated' + '_' + model_name + '.mid'))
         stream = states2music21_stream(sample, vocabs, our=False)
         # stream.show('lily')
+        if not os.path.exists(os.path.join(BASE_DIR, 'music_sheet')):
+            os.makedirs(os.path.join(BASE_DIR, 'music_sheet'))
         stream.write('musicxml.pdf',
                      os.path.join(BASE_DIR, 'music_sheet', model_name + '.xml'))
-
+        if not os.path.exists(os.path.join(MIDI_DIR, 'fhmm')):
+            os.makedirs(os.path.join(MIDI_DIR, 'fhmm'))
         stream2midi(stream, os.path.join(MIDI_DIR, 'fhmm', model_name + '.mid'))
--- main_hmm.py
+++ main_hmm.py
@@ -4,6 +4,7 @@ import music21
 import pickle
 import numpy
 import random
+import argparse
 import itertools
 import statistics
 from src import BASE_DIR, DATA_DIR, MODELS_DIR, MIDI_DIR
@@ -11,64 +12,122 @@ from src.parser.parser import states2notes, states2music21_stream
 from src.helpers import load_pickle, save_pickle, stream2midi
 from hmmlearn import hmm
 
+def prepare_dataset(dataset):
+    dataset = [numpy.array([state for state in song]).reshape(-1, 1) for song in dataset]
+    lengths = [len(song) for song in dataset]
+    return dataset, lengths
+
+def test(model, testset, test_lengths):
+    likelihoods = [model.score(song) for song in testset]
+    infs = sum(1 if math.isinf(ll) else 0 for ll in likelihoods)
+    print('#infs {} on {}-length'.format(infs, len(likelihoods)))
+    likelihoods = [ll for ll in likelihoods if not math.isinf(ll)]
+    if likelihoods != []:
+        print("AVG: {}".format(statistics.mean(likelihoods)))
+    else:
+        print("AVG: 0")
+
+def train(n_components, n_iter, n_features, trainset, trainset_lengths):
+    model = hmm.MultinomialHMM(n_components=n_components, n_iter=n_iter)
+    model.monitor_.verbose = args.verbose
+    model.n_features = n_features
+    model.fit(numpy.concatenate(trainset), trainset_lengths)
+    return model
+
 music21.environment.set('musicxmlPath', '/usr/bin/musescore')
 music21.environment.set('graphicsPath', '/usr/bin/musescore')
 music21.environment.set('musescoreDirectPNGPath', '/usr/bin/musescore')
 
-vocab = load_pickle(os.path.join(DATA_DIR, 'music21', 'vocab.pkl'))
-parsed_dataset = load_pickle(os.path.join(DATA_DIR, 'combined', 'chorales_states_dataset.pkl'))
-test_dataset = load_pickle(os.path.join(DATA_DIR, 'combined', 'beethoven_states_dataset.pkl'))
+if __name__ == "__main__":
+    print(" -- main.py HMM --\n")
+    parser = argparse.ArgumentParser(description='Train and test HMM')
+    parser.add_argument('-M', type=int, default=2, help='value M of states')
+    parser.add_argument('-N', type=int, default=15, help='Number of iterations')
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="increase output verbosity")
+    parser.add_argument("-s", "--save-model", action="store_true",
+                        help="Save the trained model")
+    parser.add_argument('--trainset-size', type=str, action='store',
+                        default='50',
+                        help='Training set size')
+    parser.add_argument('--skip-training', action='store_true')
+    parser.add_argument('--model-path', type=str, action='store',
+                        default=None,
+                        help='Load model instead of training a new one')
+    parser.add_argument("--generate", action="store_true",
+                        help="Generate a midi file")
+    parser.add_argument("--dataset-dir", type=str,
+                        default=os.path.join(DATA_DIR, 'music21'),
+                        help="Dataset base dir")
+    parser.add_argument("--trainset-name", type=str,
+                        default=os.path.join(DATA_DIR, 'music21', 'bach_states_dataset.pkl'),
+                        help="Dataset base dir")
+    parser.add_argument("--testset-name", type=str,
+                        default=None,
+                        help="Dataset base dir")
 
-# Parameters
-train_ratio = .6
-training_size = 30
-n_components = 20
-n_iter = 100
-train = False
-hmm_generate = True
-generate_original = False
+    args = parser.parse_args()
 
-training_set = parsed_dataset
-test_set = test_dataset
+    fullname = os.path.join(args.dataset_dir, args.trainset_name)
+    vocabs = load_pickle(os.path.join(args.dataset_dir, 'vocabs.pkl'))
+    dataset = load_pickle(fullname)
 
-obs_train = [numpy.array([state for state in chorale]) for chorale in training_set]
-obs_train = [state.reshape(-1, 1) for state in obs_train]
-train_lengths = [len(seq) for seq in obs_train]
+    if args.trainset_size == 'all':
+        trainset_size = len(dataset)
+        trainset = dataset
+    else:
+        trainset_size = int(args.trainset_size)
+        trainset = dataset[:trainset_size]
+    if args.testset_name is not None:
+        testset = load_pickle(os.path.join(args.dataset_dir, args.testset_name))
+    else:
+        if trainset_size < len(dataset):
+            testset = dataset[:trainset_size]
+        else:
+            print('Not enough data for the test size')
+            exit(1)
 
-obs_test = [numpy.array([state for state in chorale]) for chorale in test_set]
-obs_test = [state.reshape(-1, 1) for state in obs_test]
-test_lengths = [len(seq) for seq in obs_test]
+    # Parameters
+    trainset_size = args.trainset_size
+    n_components = args.M
+    n_iter = args.N
+    hmm_generate = args.generate
+    generate_original = False
 
-# Train the model.
-hmm.MultinomialHMM._check_input_symbols = lambda *_: True
-model_name = 'hmm_m21' + '_' + str(training_size) + '_' + str(n_components) + '_' + str(n_iter)
+    obs_train, train_lengths = prepare_dataset(trainset)
+    obs_test, test_lengths = prepare_dataset(testset)
 
-if train:
-    model = hmm.MultinomialHMM(n_components=n_components, n_iter=n_iter)
-    model.monitor_.verbose = True
-    model.n_features = len(vocab)
-    model.fit(numpy.concatenate(obs_train), train_lengths)
-    save_pickle(model, os.path.join(MODELS_DIR, 'hmm', model_name + '.pkl'))
-else:
-    model = load_pickle(os.path.join(MODELS_DIR, 'hmm', model_name + '.pkl'))
+    model_name = 'M-' + str(n_components) + '-ts-' + str(trainset_size) + '-nit-' + str(n_iter)
 
-# Print likelihoods
-likelihoods = [model.score(song) for song in obs_test]
-infs = sum(1 if math.isinf(ll) else 0 for ll in likelihoods)
-print('#infs {} on {}-length'.format(infs, len(likelihoods)))
-likelihoods = [ll if not math.isinf(ll) else 0 for ll in likelihoods]
-print("AVG: {}".format(statistics.mean(likelihoods)))
+    if not args.skip_training:
+        hmm.MultinomialHMM._check_input_symbols = lambda *_: True
+        model = train(n_components, n_iter, len(vocabs), obs_train, train_lengths)
+        if args.save_model:
+            save_pickle(model, os.path.join(MODELS_DIR, 'hmm', model_name + '.pkl'))
+    else:
+        if args.model_path is None:
+            print('SPecify the model path running this command with --model-path PATH-TO-MODEL')
+            exit(1)
+        model = load_pickle(os.path.abspath(args.model_path))
 
-# Generate song
-if hmm_generate:
-    sample, _ = model.sample(50)
-    sample = list(itertools.chain(*sample))
-    stream = states2music21_stream(sample, vocab, our=False)
-    stream.write('musicxml.pdf', os.path.join(BASE_DIR, 'music_sheet', model_name + '.xml'))
-    stream2midi(stream, os.path.join(MIDI_DIR, 'hmm', model_name + '.mid'))
+    # Print likelihoods
+    test(model, obs_test, test_lengths)
 
-if generate_original:
-    chorale_num = random.randint(0, training_size - 1)
-    sample = training_set[chorale_num]
-    stream = states2music21_stream(sample, vocab)
-    stream2midi(stream, os.path.join(MIDI_DIR, 'hmm', 'generated' + '_' + str(chorale_num) + '.mid'))
+    # Generate song
+    if hmm_generate:
+        sample, _ = model.sample(50)
+        sample = list(itertools.chain(*sample))
+        stream = states2music21_stream(sample, vocabs, our=False)
+        if not os.path.exists(os.path.join(BASE_DIR, 'music_sheet')):
+            os.makedirs(os.path.join(BASE_DIR, 'music_sheet'))
+        stream.write('musicxml.pdf',
+                     os.path.join(BASE_DIR, 'music_sheet', model_name + '.xml'))
+        if not os.path.exists(os.path.join(MIDI_DIR, 'fhmm')):
+            os.makedirs(os.path.join(MIDI_DIR, 'fhmm'))
+        stream2midi(stream, os.path.join(MIDI_DIR, 'fhmm', model_name + '.mid'))
+
+    if generate_original:
+        chorale_num = random.randint(0, trainset_size - 1)
+        sample = trainset[chorale_num]
+        stream = states2music21_stream(sample, vocabs)
+        stream2midi(stream, os.path.join(MIDI_DIR, 'hmm', 'generated' + '_' + str(chorale_num) + '.mid'))
