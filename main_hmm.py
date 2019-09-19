171da93cc238e00d1caed1c77c97a4c7617e779a
belerico <belo.fede@outlook.com>
2019-09-19 16:16:31 +0200

    Add pomegranate

--- main_hmm.py
+++ main_hmm.py
@@ -6,7 +6,7 @@ import numpy
 import random
 import argparse
 import itertools
-import statistics
+import pomegranate
 from src import BASE_DIR, DATA_DIR, MODELS_DIR, MIDI_DIR
 from src.parser.parser import states2notes, states2music21_stream
 from src.helpers import load_pickle, save_pickle, stream2midi
@@ -17,21 +17,35 @@ def prepare_dataset(dataset):
     lengths = [len(song) for song in dataset]
     return dataset, lengths
 
-def test(model, testset, test_lengths):
-    likelihoods = [model.score(song) for song in testset]
-    infs = sum(1 if math.isinf(ll) else 0 for ll in likelihoods)
-    print('#infs {} on {}-length'.format(infs, len(likelihoods)))
-    likelihoods = [ll for ll in likelihoods if not math.isinf(ll)]
-    if likelihoods != []:
-        print("AVG: {}".format(statistics.mean(likelihoods)))
+def test(model, testset, test_lengths, framework):
+    if framework == 'hmml':
+        likelihoods = numpy.array([model.score(song) for song in testset], dtype=numpy.float64)
     else:
-        print("AVG: 0")
+        likelihoods = numpy.array([model.log_probability(numpy.array(song), check_input=False) for song in testset], dtype=numpy.float64)
+    infs = sum(1 if numpy.isneginf(ll) else 0 for ll in likelihoods)
+    avg = numpy.mean(likelihoods)
+    print("AVG: {}".format(avg))
+    print('#infs {} on {}-length'.format(infs, len(likelihoods)))
+    return avg, infs
 
-def train(n_components, n_iter, n_features, trainset, trainset_lengths):
-    model = hmm.MultinomialHMM(n_components=n_components, n_iter=n_iter)
-    model.monitor_.verbose = args.verbose
-    model.n_features = n_features
-    model.fit(numpy.concatenate(trainset), trainset_lengths)
+def train(n_components, n_iter, n_features, trainset, trainset_lengths, framework):
+    if framework == 'hmml':
+        print(' -- TRAINING WITH hmmlearn --\n')
+        model = hmm.MultinomialHMM(n_components=n_components, n_iter=n_iter)
+        model.monitor_.verbose = args.verbose
+        model.n_features = n_features
+        model.fit(numpy.concatenate(trainset), trainset_lengths)
+    else:
+        print(' -- TRAINING WITH pomegranate --\n')
+        model = pomegranate.HiddenMarkovModel.from_samples(
+                    pomegranate.DiscreteDistribution,
+                    n_components=n_components,
+                    X=obs_train,
+                    algorithm='baum-welch',
+                    min_iterations=0,
+                    max_iterations=n_iter,
+                    verbose=args.verbose)
+        model.bake(verbose=args.verbose)
     return model
 
 music21.environment.set('musicxmlPath', '/usr/bin/musescore')
@@ -42,7 +56,9 @@ if __name__ == "__main__":
     print(" -- main.py HMM --\n")
     parser = argparse.ArgumentParser(description='Train and test HMM')
     parser.add_argument('-M', type=int, default=2, help='value M of states')
-    parser.add_argument('-N', type=int, default=15, help='Number of iterations')
+    parser.add_argument('-N', type=int, default=50, help='Number of iterations')
+    parser.add_argument('-F', '--framework', type=str, default='pom',
+                        help='Choose which framework you want to use to train the HMM: pom (pomegranate) or hmml (hmmlearn)')
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="increase output verbosity")
     parser.add_argument("-s", "--save-model", action="store_true",
@@ -94,14 +110,26 @@ if __name__ == "__main__":
     hmm_generate = args.generate
     generate_original = False
 
-    obs_train, train_lengths = prepare_dataset(trainset)
-    obs_test, test_lengths = prepare_dataset(testset)
+    if args.framework == 'hmml':
+        obs_train, train_lengths = prepare_dataset(trainset)
+        obs_test, test_lengths = prepare_dataset(testset)
+        obs_vocab = [numpy.array([[i] for i, _ in enumerate(vocabs)]).reshape(-1, 1)]
+        train_lengths.insert(0, len(vocabs))
+        obs_train = obs_vocab + obs_train
+    elif args.framework == 'pom':
+        obs_train = trainset
+        train_lengths = None
+        obs_test = testset
+        test_lengths = None
+    else:
+        print('You must choose a valid framework: pom (pomegranate) or hhml (hmmlearn)')
+        exit(1)
 
-    model_name = 'M-' + str(n_components) + '-ts-' + str(trainset_size) + '-nit-' + str(n_iter)
+    model_name = args.framework + '-M-' + str(n_components) + '-ts-' + str(trainset_size) + '-nit-' + str(n_iter)
 
     if not args.skip_training:
-        hmm.MultinomialHMM._check_input_symbols = lambda *_: True
-        model = train(n_components, n_iter, len(vocabs), obs_train, train_lengths)
+        # hmm.MultinomialHMM._check_input_symbols = lambda *_: True
+        model = train(n_components, n_iter, len(vocabs), obs_train, train_lengths, args.framework)
         if args.save_model:
             save_pickle(model, os.path.join(MODELS_DIR, 'hmm', model_name + '.pkl'))
     else:
@@ -111,12 +139,15 @@ if __name__ == "__main__":
         model = load_pickle(os.path.abspath(args.model_path))
 
     # Print likelihoods
-    test(model, obs_test, test_lengths)
+    test(model, obs_test, test_lengths, args.framework)
 
     # Generate song
     if hmm_generate:
-        sample, _ = model.sample(50)
-        sample = list(itertools.chain(*sample))
+        if args.framework == 'hmml':
+            sample, _ = model.sample(50)
+            sample = list(itertools.chain(*sample))
+        else:
+            sample = model.sample(length=50)
         stream = states2music21_stream(sample, vocabs, our=False)
         if not os.path.exists(os.path.join(BASE_DIR, 'music_sheet')):
             os.makedirs(os.path.join(BASE_DIR, 'music_sheet'))
