5a9f357c90f6387a8e5cf7c7b738cdbe6d2f7076
davidesamo <davide.samotti@gmail.com>
2019-09-20 19:57:34 +0200

    Added fhmm option for GUI

--- hmmlogic.py
+++ hmmlogic.py
@@ -8,10 +8,22 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.secret_key = "secret"
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET'])
+@app.route('/index', methods = ['GET'])
 def index():
-    return render_template("index.html", link1 = "/music_generator", link2 = "/train_hmm")
+    if request.method == "GET":
+        parameters = request.args
+        type = parameters.get('hmm_type')
+        train = parameters.get('train')
+        generate = parameters.get('generate')
+        session['type'] = type
+        if train == "train":
+            result = render_template("train_hmm.html", link = "/", type = type)
+        elif generate == "generate":
+            result = render_template("music_generator.html", link = "/", type = type)
+        else:
+            result = render_template("index.html")
+    return result
 
 @app.route('/train_hmm')
 def train_hmm():
@@ -25,11 +37,17 @@ def music_generator():
 def sample():
     image = None
     midi = None
+    type = session['type']
     framework = request.form.get('generator_framework')
     if request.method == "POST":
         model_name = request.files['model'].filename
-        model = hmm.load_pickle(os.path.join(hmm.MODELS_DIR, 'hmm', model_name))
-        image, midi = hmm.generate_sample(model, model_name, framework)
+        if type == "hmm":
+            model = hmm.load_pickle(os.path.join(hmm.MODELS_DIR, 'hmm', model_name))
+            image, midi = hmm.generate_sample_hmm(model, model_name, framework)
+        else:
+            model = hmm.load_pickle(os.path.join(hmm.MODELS_DIR, 'fhmm', model_name))
+            image, midi = hmm.generate_sample_fhmm(model, model_name)
+    print(image,midi)
     return render_template("music_generator.html", image = image, link = "/", midi = midi)
 
 @app.route('/train', methods = ['POST'])
@@ -37,50 +55,75 @@ def training():
     flag_test = False
     if request.method == "POST":
         parameters = request.form
-        framework = parameters.get("framework")
+        type = session['type']
+        if type == "hmm":
+            framework = parameters.get("framework")
+            session['framework'] = framework
+        else:
+            K = int(parameters.get('k_value'))
         size = parameters.get("size")
         if size != "all":
             size = int(parameters.get("size"))
         iter = int(parameters.get("n_iter"))
         components = int(parameters.get("n_components"))
-        trainset, testset, vocabs = hmm.init(os.path.join(hmm.DATA_DIR, 'music21', 'bach_states_dataset.pkl'), size)
-        if framework == "hmml":
-            obs_train, train_lengths = hmm.prepare_dataset(trainset)
-            obs_vocab = [numpy.array([[i] for i, _ in enumerate(vocabs)]).reshape(-1, 1)]
-            train_lengths.insert(0, len(vocabs))
-            obs_train = obs_vocab + obs_train
-        else:
-            obs_train = trainset
-            train_lengths = None
 
-        model, name = hmm.train(components, iter, len(vocabs), obs_train, train_lengths, size, framework)
-        session['framework'] = framework
+        if type == "hmm":
+            trainset, testset, vocabs = hmm.init(os.path.join(hmm.DATA_DIR, 'music21', 'bach_states_dataset.pkl'), size, type)
+            if framework == "hmml":
+                obs_train, train_lengths = hmm.prepare_dataset(trainset)
+                obs_vocab = [numpy.array([[i] for i, _ in enumerate(vocabs)]).reshape(-1, 1)]
+                train_lengths.insert(0, len(vocabs))
+                obs_train = obs_vocab + obs_train
+            else:
+                obs_train = trainset
+                train_lengths = None
+            model, name = hmm.train_hmm(components, iter, len(vocabs), obs_train, train_lengths, size, framework)
+        else:
+            trainset, testset, vocabs = hmm.init(os.path.join(hmm.DATA_DIR, 'chorales', 'music21', 'chorales_states_dataset.pkl'), size, type)
+            D = len(vocabs)
+            model, name = hmm.train_fhmm(D, components, K, iter, size, trainset)
+
         session['model'] = name
         session['size'] = size
         if model is not None:
             flag_test = True
 
-    return render_template("train_hmm.html", link="/", flag = True, flag_test = flag_test)
+    return render_template("train_hmm.html", link="/", flag = True, flag_test = flag_test, type = type)
 
 @app.route('/test', methods = ['POST'])
 def testing():
     model_name = session['model'] + '.pkl'
     size = session['size']
-    framework = session['framework']
-    model = hmm.load_pickle(os.path.join(hmm.MODELS_DIR, 'hmm', model_name))
+    type = session['type']
+    infs = means = result = ""
+    if type == "hmm":
+        framework = session['framework']
+        model = hmm.load_pickle(os.path.join(hmm.MODELS_DIR, 'hmm', model_name))
+    else:
+        model = hmm.load_pickle(os.path.join(hmm.MODELS_DIR, 'fhmm', model_name))
+
     if request.method == "POST":
         if request.files['testset'].filename != "":
             pkl = request.files['testset']
             testset = hmm.load_pickle(os.path.join(hmm.DATA_DIR, 'music21', pkl.filename))
         else:
-            dataset = hmm.load_pickle(os.path.join(hmm.DATA_DIR, 'music21', 'bach_states_dataset.pkl'))
-            testset = dataset[size:]
-    if framework == "hmml":
-        obs_test, test_lengths = hmm.prepare_dataset(testset)
+            if type == "hmm":
+                dataset = hmm.load_pickle(os.path.join(hmm.DATA_DIR, 'music21', 'bach_states_dataset.pkl'))
+                testset = dataset[size:]
+            else:
+                dataset = hmm.load_pickle(os.path.join(hmm.DATA_DIR, 'chorales', 'music21', 'chorales_states_dataset.pkl'))
+                testset = dataset[size:]
+
+    if type == "hmm":
+        if framework == "hmml":
+            obs_test, test_lengths = hmm.prepare_dataset(testset)
+        else:
+            obs_test = testset
+        infs, means = hmm.test_hmm(model, obs_test, framework)
     else:
-        obs_test = testset
-    infs, means = hmm.test(model, obs_test, framework)
-    return render_template("train_hmm.html", link="/", flag_test = True, infs = infs, means = means)
+        result = hmm.test_fhmm(model, testset)
+
+    return render_template("train_hmm.html", link="/", flag_test = True, infs = infs, means = means, result = result, type = type)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main_gui.py
+++ main_gui.py
@@ -1,22 +1,22 @@
 import os
 import math
 import music21
-import pickle
 import numpy
-import random
-import argparse
 import itertools
-import statistics
 import pomegranate
+from factorial_hmm import FullDiscreteFactorialHMM
 from src import BASE_DIR, DATA_DIR, MODELS_DIR, MIDI_DIR
 from src.parser.parser import states2notes, states2music21_stream
 from src.helpers import load_pickle, save_pickle, stream2midi
 from hmmlearn import hmm
 
 
-def init(data_path, size):
+def init(data_path, size, type):
     vocabs = load_pickle(os.path.join(DATA_DIR, 'music21', 'vocabs.pkl'))
     dataset = load_pickle(data_path)
+
+    if type == "fhmm":
+        dataset = numpy.array(list(map(numpy.array, dataset)))
 
     if size == 'all':
         trainset = dataset
@@ -37,7 +37,7 @@ def prepare_dataset(dataset):
     lengths = [len(song) for song in dataset]
     return dataset, lengths
 
-def test(model, testset, framework):
+def test_hmm(model, testset, framework):
     if framework == "hmml":
         print("Using hmml")
         likelihoods = numpy.array([model.score(song) for song in testset], dtype=numpy.float64)
@@ -52,7 +52,7 @@ def test(model, testset, framework):
     infs_string = '#infs {} on {}-length'.format(infs, len(likelihoods))
     return infs_string, result_string
 
-def train(n_components, n_iter, n_features, trainset, trainset_lengths, size, framework):
+def train_hmm(n_components, n_iter, n_features, trainset, trainset_lengths, size, framework):
     if framework == "hmml":
         print("Using hmml")
         model = hmm.MultinomialHMM(n_components=n_components, n_iter=n_iter)
@@ -74,7 +74,7 @@ def train(n_components, n_iter, n_features, trainset, trainset_lengths, size, fr
     save_pickle(model, os.path.join(MODELS_DIR, 'hmm', model_name + '.pkl'))
     return model, model_name
 
-def generate_sample(model, model_name, framework):
+def generate_sample_hmm(model, model_name, framework):
     vocabs = load_pickle(os.path.join(DATA_DIR, 'music21', 'vocabs.pkl'))
     if framework == "hmml":
         print("Using hmml")
@@ -87,14 +87,73 @@ def generate_sample(model, model_name, framework):
     if not os.path.exists(os.path.join(BASE_DIR, 'music_sheet')):
         os.makedirs(os.path.join(BASE_DIR, 'music_sheet'))
     conv = music21.converter.subConverters.ConverterLilypond()
-    conv.write(stream, fmt='lilypond', fp=os.path.join(BASE_DIR, 'static', 'scores', model_name), subformats=['png'])
-    image = os.path.join('static', 'scores', model_name) + ".png"
+    conv.write(stream, fmt='lilypond', fp=os.path.join(BASE_DIR, 'static', 'scores', 'hmm', model_name), subformats=['png'])
+    image = os.path.join('static', 'scores', 'hmm', model_name) + ".png"
     files = os.listdir(os.path.join(BASE_DIR, 'static', 'scores'))
     for item in files:
         if item.endswith(".eps") or item.endswith(".count") or item.endswith(".tex") or item.endswith(".texi") or item.endswith(".pkl"):
-            os.remove(os.path.join(BASE_DIR, 'static', 'scores', item))
+            os.remove(os.path.join(BASE_DIR, 'static', 'scores', 'hmm', item))
     if not os.path.exists(os.path.join(MIDI_DIR, 'hmm')):
         os.makedirs(os.path.join(MIDI_DIR, 'hmm'))
-    stream2midi(stream, os.path.join(BASE_DIR, 'static', 'midi', model_name + '.mid'))
-    midi = os.path.join('static' ,'midi', model_name + '.mid')
+    stream2midi(stream, os.path.join(BASE_DIR, 'static', 'midi', 'hmm', model_name + '.mid'))
+    midi = os.path.join('static' ,'midi', 'hmm', model_name + '.mid')
+    return image, midi
+
+def train_fhmm(D, M, K, n_iterations, size, trainset, random_seed=42):
+    print(" -- Training FHMM --\n")
+
+    params = {'hidden_alphabet_size': K, 'n_hidden_chains': M,
+              'observed_alphabet_size': D, 'n_observed_chains': 1,
+              'initial_hidden_state': numpy.zeros((M, K)),
+              'transition_matrices': numpy.zeros((M, K, K)),
+              'obs_given_hidden': numpy.zeros([K] * M + [D])}
+
+    random_state = numpy.random.RandomState(random_seed)
+    for i in range(M):
+        params['transition_matrices'][i][:][:] = [[1 / K] * K] * K
+        params['initial_hidden_state'][i, :] = [1 / K] * K
+
+    for st in itertools.product(*[range(K)] * M):
+        R = random_state.rand(D)
+        R /= R.sum()
+        params['obs_given_hidden'][list(st) + [Ellipsis]] = R
+
+    hmm = FullDiscreteFactorialHMM(params=params, n_steps=100, calculate_on_init=True)
+    hmm = hmm.EM(trainset, n_iterations=n_iterations)
+
+    model_name = f"K-{K}-M-{M}-ts-{size}-nit-{n_iterations}"
+    save_pickle(hmm, os.path.join(MODELS_DIR, 'fhmm', model_name + ".pkl"))
+
+    return hmm, model_name
+
+def test_fhmm(model, testset):
+    print(" -- Testing FHMM --\n")
+    log_likelihoods = numpy.array(
+        [model.Forward(numpy.array(testset[i]))[2] for i, sequence in
+         enumerate(testset)])
+    result = f"Test likelihood {numpy.mean(log_likelihoods)}"
+
+    return result
+
+def generate_sample_fhmm(model, model_name):
+    print(" -- Generating MIDI --\n")
+    vocabs = load_pickle(os.path.join(DATA_DIR, 'chorales', 'music21', 'vocabs.pkl'))
+    _, sample = model.Simulate()
+    sample = list(itertools.chain(*sample))[:50]
+    stream = states2music21_stream(sample, vocabs, our=False)
+    # stream.show('lily')
+    if not os.path.exists(os.path.join(BASE_DIR, 'music_sheet')):
+        os.makedirs(os.path.join(BASE_DIR, 'music_sheet'))
+    conv = music21.converter.subConverters.ConverterLilypond()
+    conv.write(stream, fmt='lilypond', fp=os.path.join(BASE_DIR, 'static', 'scores', 'fhmm', model_name), subformats=['png'])
+    image = os.path.join('static', 'scores', 'fhmm',  model_name) + ".png"
+    files = os.listdir(os.path.join(BASE_DIR, 'static', 'scores', 'fhmm'))
+    for item in files:
+        if item.endswith(".eps") or item.endswith(".count") or item.endswith(".tex") or item.endswith(
+                ".texi") or item.endswith(".pkl"):
+            os.remove(os.path.join(BASE_DIR, 'static', 'scores', 'fhmm', item))
+    if not os.path.exists(os.path.join(MIDI_DIR, 'fhmm')):
+        os.makedirs(os.path.join(MIDI_DIR, 'fhmm'))
+    stream2midi(stream, os.path.join(BASE_DIR, 'static', 'midi', 'fhmm', model_name + '.mid'))
+    midi = os.path.join('static', 'midi', 'fhmm', model_name + '.mid')
     return image, midi
